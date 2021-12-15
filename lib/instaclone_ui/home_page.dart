f4ef8bc0230227b2000a745d499f4d9b83d37ec3
parkgyeonghwan <ghksdlajwu@gmail.com>
2021-12-15 09:17:22 +0900

    플로팅액션버튼 테두리 추가

--- lib/instaclone_ui/home_page.dart
+++ lib/instaclone_ui/home_page.dart
@@ -14,48 +14,47 @@ class HomePage extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
+                  ProfileCard(
+                    imgUrl: 'https://www.sisaweek.com/news/photo/202007/135804_126037_531.jpg',
+                    name: '박경환',
+                  ),
+
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
                       children: [
-                        Stack(
-                          children: [
-                            SizedBox(
-                              width: 80,
-                              height: 80,
-                              child: CircleAvatar(
-                                backgroundImage: NetworkImage(
-                                    'https://topclass.chosun.com/news_img/2008/2008_008_4.jpg'),
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Container(
+                              width: 90,
+                              height: 90,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
                               ),
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              right: 0,
-                              child: Container(
-                                width: 35,
-                                height: 35,
-                                // alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  shape: BoxShape.circle,
-                                ),
-                                child: Center(
-                                  child: SizedBox(
-                                    width: 30,
-                                    height: 30,
-                                    child: FloatingActionButton(
-                                        onPressed: () {},
-                                        backgroundColor: Colors.blue,
-                                        child: const Icon(Icons.add)),
+                              child: Center(
+                                child: Container(
+                                  width: 80,
+                                  height: 80,
+                                  child: CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        'https://cdn.mhns.co.kr/news/photo/202008/415511_538664_2810.jpg'),
                                   ),
                                 ),
                               ),
-
                             ),
-
-                          ],
+                          ),
                         ),
-                        Text("박경환"),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text("초아"),
+                        ),
                       ],
                     ),
                   ),
@@ -63,15 +62,38 @@ class HomePage extends StatelessWidget {
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
                       children: [
-                        SizedBox(
-                          width: 80,
-                          height: 80,
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                'https://cdn.mhns.co.kr/news/photo/202008/415511_538664_2810.jpg'),
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Container(
+                              width: 90,
+                              height: 90,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Container(
+                                  width: 80,
+                                  height: 80,
+                                  child: CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        'https://image.newdaily.co.kr/site/data/img/2020/01/03/2020010300158_0.jpg'),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                        Text("초아"),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          child: Text("보영"),
+                        ),
                       ],
                     ),
                   ),
@@ -79,15 +101,38 @@ class HomePage extends StatelessWidget {
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
                       children: [
-                        SizedBox(
-                          width: 80,
-                          height: 80,
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                'https://nimage.g-enews.com/phpwas/restmb_allidxmake.php?idx=5&simg=2019111315302604184c4c55f9b3d591019584.jpg'),
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Container(
+                              width: 90,
+                              height: 90,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Container(
+                                  width: 80,
+                                  height: 80,
+                                  child: CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        'https://thumb.mt.co.kr/06/2021/04/2021042313548283490_1.jpg/dims/optimize/'),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                        Text("보영"),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          child: Text("민아"),
+                        ),
                       ],
                     ),
                   ),
@@ -95,31 +140,38 @@ class HomePage extends StatelessWidget {
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
                       children: [
-                        SizedBox(
-                          width: 80,
-                          height: 80,
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                'http://img.tf.co.kr/article/home/2014/11/13/201425201415844176.jpg'),
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Container(
+                              width: 90,
+                              height: 90,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Container(
+                                  width: 80,
+                                  height: 80,
+                                  child: CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        'https://blog.kakaocdn.net/dn/cAjagW/btqyAM2wMqL/48iIMfaQF9Uk2Qu3oKi721/img.jpg'),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                        Text("민아"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          width: 80,
-                          height: 80,
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                'https://blog.kakaocdn.net/dn/cAjagW/btqyAM2wMqL/48iIMfaQF9Uk2Qu3oKi721/img.jpg'),
-                          ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          child: Text("은하"),
                         ),
-                        Text("은하"),
                       ],
                     ),
                   ),
@@ -128,6 +180,7 @@ class HomePage extends StatelessWidget {
             ),
             const Divider(
               thickness: 0,
+              height: 1,
               color: Colors.grey,
             ),
             Padding(
@@ -210,7 +263,10 @@ class HomePage extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text("10개댓글",style: TextStyle(color: Colors.grey),),
+                  child: Text(
+                    "10개댓글",
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 )
               ],
             ),
@@ -294,14 +350,105 @@ class HomePage extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text("10개댓글",style: TextStyle(color: Colors.grey),),
+                  child: Text(
+                    "10개댓글",
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 )
               ],
             )
           ],
-
         ),
       ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  final String imgUrl;
+  final String name;
+
+  const ProfileCard({
+    required this.imgUrl,
+    required this.name,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          width: 90,
+                          height: 90,
+                          child: Center(
+                            child: Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                CircleAvatar(
+                                  radius: 40,
+                                  backgroundImage: NetworkImage(
+                                      imgUrl),
+                                ),
+                                Positioned(
+                                  bottom: -10,
+                                  right: -10,
+                                  child: Container(
+                                    height: 45,
+                                    width: 45,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: Center(
+                                      child: SizedBox(
+                                        height: 35,
+                                        width: 35,
+                                        child: FloatingActionButton(
+                                          onPressed: () {},
+                                          backgroundColor: Colors.blue,
+                                          child: Icon(Icons.add),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(name),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
